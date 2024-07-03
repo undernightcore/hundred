1d3daa2f8c334d2773f7bbe89c40e4291b7af9a3
Javier Martí Valcárcel <undernightcore@gmail.com>
2024-07-03 14:55:09 +0200

    feat: remove models dependencies

--- src/services/tts.ts
+++
@@ -1,7 +0,0 @@
-import { pipeline } from '@xenova/transformers';
-
-export async function textToSpeech(message: string) {
-    let pipe = await pipeline('sentiment-analysis', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english');
-
-    return pipe('I love you dude!');
-}
